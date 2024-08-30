7a92fd854685e2daf21c3fe67f5e0ecf106f70b7
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-08-30 17:22:36 +0000

    more documentation

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Basic Flask app
+Basic Flask app with internationalization support.
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
