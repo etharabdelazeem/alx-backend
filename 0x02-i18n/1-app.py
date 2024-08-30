d62ca8acb2e4eabd4813c72a9a7d129e01b93cd9
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-08-30 15:49:18 +0000

    render the right index page

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -22,7 +22,7 @@ babel = Babel(app)
 @app.route('/')
 def index() -> str:
     """renders the index page"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
