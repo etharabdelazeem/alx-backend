83a977f7f26c3d0713482f3a5d733bacfb81b3ab
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-08-30 16:16:25 +0000

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -20,7 +20,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale()-> str:
     """Determine the best match for supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
