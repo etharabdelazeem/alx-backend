54babeab0b456c9918ba4166428296c038271d93
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-08-30 14:07:27 +0000

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
+def index() -> str:
     """renders the index page"""
-    return render_template('index.html')
+    return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
